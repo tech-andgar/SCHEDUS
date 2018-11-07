5e698e51809cd12e36b0ac3be9f91cec5d8c9abc
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-07 12:38:23 -0500

    Index views

--- views/all/footer.php
+++ views/all/footer.php
@@ -1,8 +1,9 @@
 
     <!-- FOOTER -->
-    <footer class="app-footer">
-        <span><a href="http://www.incanatoit.com/">IncanatoIT</a> &copy; 2017</span>
-        <span class="ml-auto">Desarrollado por <a href="http://www.incanatoit.com/">IncanatoIT</a></span>
+    <footer class="app-footer" style="margin-left:0;">
+        <span><a href="#">SCHEDUS</a> &copy; 2018</span>
+        <span class="ml-auto mr-2">Desarrollado por <a href="https://github.com/andgar2010">Andres Garcia</a></span>
+        <span class="my-auto"><a href="https://github.com/johannbulls">Johann amaya</a></span>
     </footer>
     <!-- END FOOTER -->
 
--- views/all/header.php
+++ views/all/header.php
@@ -1,50 +1,16 @@
 <header class="app-header navbar">
-        <button class="navbar-toggler mobile-sidebar-toggler d-lg-none mr-auto" type="button">
-          <span class="navbar-toggler-icon"></span>
-        </button>
         <a class="navbar-brand" href="#"></a>
-        <button class="navbar-toggler sidebar-toggler d-md-down-none" type="button">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <ul class="nav navbar-nav d-md-down-none">
-            <li class="nav-item px-3">
-                <a class="nav-link" href="#">Escritorio</a>
-            </li>
-            <li class="nav-item px-3">
-                <a class="nav-link" href="#">Configuraciones</a>
-            </li>
-        </ul>
         <ul class="nav navbar-nav ml-auto">
-            <li class="nav-item d-md-down-none">
-                <a class="nav-link" href="#" data-toggle="dropdown">
-                    <i class="icon-bell"></i>
-                    <span class="badge badge-pill badge-danger">5</span>
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle nav-link mr-5" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-home"></i><span class="d-md-down-none"> Inicio </span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right">
-                    <div class="dropdown-header text-center">
-                        <strong>Notificaciones</strong>
+                    <div class="dropdown-header text-center fas fa-cog">
+                        <strong></strong>
                     </div>
-                    <a class="dropdown-item" href="#">
-                        <i class="fa fa-envelope-o"></i> Ingresos
-                        <span class="badge badge-success">3</span>
-                    </a>
-                    <a class="dropdown-item" href="#">
-                        <i class="fa fa-tasks"></i> Ventas
-                        <span class="badge badge-danger">2</span>
-                    </a>
-                </div>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                    <img src="././assets/images/avatars/6.jpg" class="img-avatar" alt="admin@bootstrapmaster.com">
-                    <span class="d-md-down-none">admin </span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <div class="dropdown-header text-center">
-                        <strong>Cuenta</strong>
-                    </div>
-                    <a class="dropdown-item" href="#"><i class="fa fa-user"></i> Perfil</a>
-                    <a class="dropdown-item" href="#"><i class="fa fa-lock"></i> Cerrar sesión</a>
+                    <a class="dropdown-item" href="#"><i class="fas fa-home"></i> Inicio</a>
+                    <a class="dropdown-item" href="#"><i class="fas fa-sign-in-alt"></i> Iniciar sesión</a>
                 </div>
             </li>
         </ul>
