d7151ef833a7625fd2fa451ed03a3a4d88a75d65
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-08 08:35:35 -0500

    Auto stash before merge of "Redesign-Dashboard" and "copy-temp"

---
+++ views/all/header.php
@@ -0,0 +1,17 @@
+<header class="app-header navbar">
+        <a class="navbar-brand" href="?c=Inicio&m=Index"></a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item dropdown ">
+                <a class="nav-link dropdown-toggle nav-link mr-5" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-home"></i><span class="d-md-down-none"> Inicio </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right text-center">
+                    <div class="dropdown-header">
+                        <strong><i class=" fas fa-cog"></i></strong>
+                    </div>
+                    <a class="dropdown-item" href="?c=inicio&m=index"><i class="fas fa-home"></i> Inicio</a>
+                    <a class="dropdown-item" href="#" data-toggle="modal" data-target="#exampleModalCenter"><i class="fas fa-sign-in-alt"></i> Iniciar sesión</a>
+                </div>
+            </li>
+        </ul>
+    </header>
