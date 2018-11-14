1977bccdb578647a9673c2508cedb7bbea2ba4e0
Andrés García <afgarcia0479@misena.edu.co>
2018-11-14 17:51:19 -0500

    Clean Code Head and Ficha

--- views/Lider/Fichas.php
+++ views/Lider/Fichas.php
@@ -5,10 +5,11 @@
 		<li class="breadcrumb-item">Inicio</li>
 		<li class="breadcrumb-item"><a href="#">Lider</a></li>
 		<li class="breadcrumb-item active">Fichas</li>
-	<div class="container-fluid">
+		<div class="container-fluid">
 	</ol>
+	<div class="container-fluid">
 		<div class="card">
-		<!-- Ejemplo de tabla Listado -->
+			<!-- Ejemplo de tabla Listado -->
 			<div class="card-header ">
 				<i class="fa fa-align-justify"></i> Fichas
 				<button type="button" class="btn btn-secondary " data-toggle="modal" data-target="#Agregar_Ficha">
@@ -21,34 +22,34 @@
 						<div class="input-group">
 							<input type="text" id="texto" name="texto" class="form-control" placeholder="Texto a buscar">
 						</div>
-							<button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
+						<button type="submit" class="btn btn-primary"><i class="fa fa-search"></i> Buscar</button>
 					</div>
 				</div>
 				<div class="table-responsive">
-					<table class="table table-responsive-sm table-bordered table-striped table-sm mt-5" id="tableInstructores">
+					<table class="table table-responsive-sm table-bordered table-striped table-sm mt-5" id="tableFichas">
 						<thead>
 							<tr>
-						<th>Ficha</th>
-						<th>Programa</th>
-						<th>Nivel</th>
-						<th>Estado</th>
+								<th>Ficha</th>
+								<th>Programa</th>
+								<th>Nivel</th>
+								<th>Estado</th>
 								<th class="text-center">Actualizar</th>
 							</tr>
 						</thead>
-				<tbody>
-					<?php foreach ($data['fichas'] as $ficha) { ?>
-					<tr>
-						<td>
-							<?php echo $ficha->num_ficha; ?>
-						</td>
-						<td>
-							<?php echo $ficha->name_programa_formacion; ?>
-						</td>
-						<td>
-							<?php echo $ficha->name_nivel_programa_formacion; ?>
-						</td>
-						<td>
-							<?php
+						<tbody>
+							<?php foreach ($data['fichas'] as $ficha) { ?>
+							<tr>
+								<td>
+									<?php echo $ficha->num_ficha; ?>
+								</td>
+								<td>
+									<?php echo $ficha->name_programa_formacion; ?>
+								</td>
+								<td>
+									<?php echo $ficha->name_nivel_programa_formacion; ?>
+								</td>
+								<td>
+									<?php
 								switch ($ficha->name_estado_ficha) {
 									case 'Cancelado':
 										echo '<div class="p-2 bg-danger text-white rounded">' . $ficha->name_estado_ficha . '</div>';
@@ -66,25 +67,26 @@
 										echo '<div class="p-2 bg-dark text-white rounded">No pudo obtener estado de formacion ' . $ficha->name_estado_ficha . '</div>';
 									break;
 								}?>
-						</td>
-						<td style="padding-bottom: 0px;padding-top: 10px;">
-							<div class="updateDataFicha" data-toggle="modal" data-target="#updateDataFicha" id-ficha="<?php echo $ficha->id_ficha; ?>">
-								<i class="far fa-edit fa-lg"></i>
-							</div>
-						</td>
-					</tr>
-					<?php } ?>
-				</tbody>
-			</table>
+								</td>
+								<td style="padding-bottom: 0px;padding-top: 10px;">
+									<div class="updateDataFicha" data-toggle="modal" data-target="#updateDataFicha" id-ficha="<?php echo $ficha->id_ficha; ?>">
+										<i class="far fa-edit fa-lg"></i>
+									</div>
+								</td>
+							</tr>
+							<?php } ?>
+						</tbody>
+					</table>
 				</div>
 			</div>
 		</div>
-		<!-- Fin ejemplo de tabla Listado -->
 	</div>
-	<!-- Fin del modal Eliminar -->
+
+	<!-- Fin ejemplo de tabla Listado -->
 </main>
-<!-- /Fin del contenido principal -->
+<!-- Fin del modal Eliminar -->
 </div>
+<!-- /Fin del contenido principal -->
 
 <!-- Modal Agregar nueva Ficha-->
 <div class="modal fade" id="Agregar_Ficha" tabindex="-1" role="dialog" aria-hidden="true">
@@ -107,15 +109,15 @@
 						</div>
 						<div class="form-group">
 
-						<h4>Programa:</h4>
-						<select class="adsi-css mb-3" name="cod_programa_formacion" required>
-							<option value="">Programas</option>
-							<?php foreach ($data['programaFormacion'] as $programa) { ?>
-							<option value="<?php echo $programa->id_programa_formacion; ?>">
-								<?php echo $programa->name_programa_formacion; ?>
-							</option>
-							<?php } ?>
-						</select>
+							<h4>Programa:</h4>
+							<select class="adsi-css mb-3" name="cod_programa_formacion" required>
+								<option value="">Programas</option>
+								<?php foreach ($data['programaFormacion'] as $programa) { ?>
+								<option value="<?php echo $programa->id_programa_formacion; ?>">
+									<?php echo $programa->name_programa_formacion; ?>
+								</option>
+								<?php } ?>
+							</select>
 						</div>
 
 						<hr>
@@ -142,40 +144,40 @@
 			<div class="modal-body">
 				<div class="d-flex justify-content-center text-center">
 					<form method="POST" action="?c=Lider&m=updateDataFicha" class="form-signin">
-							<table>
-								<tr>
-									<td>
-										<h4 style="margin-bottom: 0px;" class="mr-5" for="txt_num_ficha">Numero de la Ficha:</h4>
-									</td>
-									<td>
-										<input type="text" name="txt_num_ficha" id="txt_num_ficha" class="adsi-css" aria-describedby="helpIdNumFicha" readonly>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<h4 style="margin-bottom: 0px;">Estado:</h4>
-										<small id="helpIdNumFicha" class="text-muted">Selecciona estado de ficha</small>
-									</td>
-									<td>
-										<select id="list-estadoFicha" class="adsi-css mb-3" name="id_estado_ficha" required>
-										</select>
-									</td>
-								</tr>
-								<tr>
-									<td>
-										<h4 style="margin-bottom: 0px;">Programa:</h4>
-										<small id="helpIdNumFicha" class="text-muted">Selecciona programa de formación</small>
-									</td>
-									<td>
-										<select class="adsi-css mb-3" id="list-programa" name="id_programa_formacion" required>
-										</select>
-									</td>
-								</tr>
-							</table>
-							<div class="modal-body">
-								<button class="btn-rounded" type="submit" style="width:110px">Actualizar</button>
-							</div>
-						</form>
+						<table>
+							<tr>
+								<td>
+									<h4 style="margin-bottom: 0px;" class="mr-5" for="txt_num_ficha">Numero de la Ficha:</h4>
+								</td>
+								<td>
+									<input type="text" name="txt_num_ficha" id="txt_num_ficha" class="adsi-css" aria-describedby="helpIdNumFicha" readonly>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<h4 style="margin-bottom: 0px;">Estado:</h4>
+									<small id="helpIdNumFicha" class="text-muted">Selecciona estado de ficha</small>
+								</td>
+								<td>
+									<select id="list-estadoFicha" class="adsi-css mb-3" name="id_estado_ficha" required>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<h4 style="margin-bottom: 0px;">Programa:</h4>
+									<small id="helpIdNumFicha" class="text-muted">Selecciona programa de formación</small>
+								</td>
+								<td>
+									<select class="adsi-css mb-3" id="list-programa" name="id_programa_formacion" required>
+									</select>
+								</td>
+							</tr>
+						</table>
+						<div class="modal-body">
+							<button class="btn-rounded" type="submit" style="width:110px">Actualizar</button>
+						</div>
+					</form>
 				</div>
 			</div>
 		</div>
@@ -198,10 +200,10 @@
 					<form method="post" action="#" class="form-signin">
 						<div class="form-group">
 							<div class="form-group">
-
-								<label for="id_ficha"><h5>Ficha</h5>
-									<br/>
-									<input type="text" name="id_ficha" list="id_ficha" class="adsi-css" placeholder="Número de ficha"/>
+								<label for="id_ficha">
+									<h5>Ficha</h5>
+									<br>
+									<input type="text" name="id_ficha" list="id_ficha" class="adsi-css" placeholder="Número de ficha" />
 									<datalist id="id_ficha">
 										<label> or select one from the list:
 											<div id="form_select_ficha"></div>
@@ -241,85 +243,85 @@
 
 	$(document).ready(function () {
 
-	    $("#tableFichas").DataTable({
-	        "language": {
-	            "sProcessing": "Procesando...",
-	            "sLengthMenu": "Mostrar _MENU_ registros",
-	            "sZeroRecords": "No se encontraron resultados",
-	            "sEmptyTable": "Ningún dato disponible en esta tabla",
-	            "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-	            "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
-	            "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-	            "sInfoPostFix": "",
-	            "sSearch": "Buscar:",
-	            "sUrl": "",
-	            "sInfoThousands": ",",
-	            "sLoadingRecords": "Cargando...",
-	            "oPaginate": {
-	                "sFirst": "Primero",
-	                "sLast": "Último",
-	                "sNext": "Siguiente",
-	                "sPrevious": "Anterior"
-	            },
-	            "oAria": {
-	                "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-	                "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-	            }
-	        },
-	        "dom": // Insertar objeto tabla por formato:
-	            // Encabezado de la tabla -- l->Num registros por pagina, f-> barra de filtro
-	            "<'row'<'col-sm-6'f><'col-sm-6'l>>" +
-	            // Cuerpo de la tabla -- t-> tabla, r (no aun entiendo)
-	            "<'row'<'col-sm-12 table-responsive d-flex justify-content-center'tr>>" +
-	            // Seccion estado de la tabla -- i-> info de tabla, p-> num Paginas por dividir registros
-	            "<'row'<'col-sm-2'><'col-sm-10'i><'col-sm-3'><'col-sm-7'p>>" +
-	            // Pie de la tabla -- B-> Botones de exportar
-	            "<'row'<'col-sm-12'B>>",
-	        buttons: [
-	            'copy',
-	            'excel',
-	            'pdf'
-	        ]
-	        //buttons: [
-	        //	'copyHtml5',
-	        //	'excelHtml5',
-	        //	'csvHtml5',
-	        //	'pdfHtml5'
-	        //]
-	    });
+		$("#tableFichas").DataTable({
+			"language": {
+				"sProcessing": "Procesando...",
+				"sLengthMenu": "Mostrar _MENU_ registros",
+				"sZeroRecords": "No se encontraron resultados",
+				"sEmptyTable": "Ningún dato disponible en esta tabla",
+				"sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+				"sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+				"sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+				"sInfoPostFix": "",
+				"sSearch": "Buscar:",
+				"sUrl": "",
+				"sInfoThousands": ",",
+				"sLoadingRecords": "Cargando...",
+				"oPaginate": {
+					"sFirst": "Primero",
+					"sLast": "Último",
+					"sNext": "Siguiente",
+					"sPrevious": "Anterior"
+				},
+				"oAria": {
+					"sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+				}
+			},
+			"dom": // Insertar objeto tabla por formato:
+				// Encabezado de la tabla -- l->Num registros por pagina, f-> barra de filtro
+				"<'row'<'col-sm-6'f><'col-sm-6'l>>" +
+				// Cuerpo de la tabla -- t-> tabla, r (no aun entiendo)
+				"<'row'<'col-sm-12 table-responsive d-flex justify-content-center'tr>>" +
+				// Seccion estado de la tabla -- i-> info de tabla, p-> num Paginas por dividir registros
+				"<'row'<'col-sm-4'><'col-sm-8'i><'col-sm-4'><'col-sm-7'p>>" +
+				// Pie de la tabla -- B-> Botones de exportar
+				"<'row'<'col-sm-12'B>>",
+			buttons: [
+				'copy',
+				'excel',
+				'pdf'
+			]
+			//buttons: [
+			//	'copyHtml5',
+			//	'excelHtml5',
+			//	'csvHtml5',
+			//	'pdfHtml5'
+			//]
+		});
 
 
 		// Change status of ficha from button
-	    $(".statusChange").click(function () {
-	        var statetext = $(this).attr('name-state');
-	        var state_id = $(this).attr('id-state');
-	        var id_Ficha = $(this).attr('id-Ficha');
-	        $.ajax({
-	            type: 'POST',
-	            url: '?c=Lider&m=changeStatusFicha',
-	            data: {
-	                statetext: statetext,
-	                state_id: state_id,
-	                id_Ficha: id_Ficha
-	            },
-	            success(response) {
-	                location.reload();
-	            }
-	        });
-	    });
+		$(".statusChange").click(function () {
+			var statetext = $(this).attr('name-state');
+			var state_id = $(this).attr('id-state');
+			var id_Ficha = $(this).attr('id-Ficha');
+			$.ajax({
+				type: 'POST',
+				url: '?c=Lider&m=changeStatusFicha',
+				data: {
+					statetext: statetext,
+					state_id: state_id,
+					id_Ficha: id_Ficha
+				},
+				success(response) {
+					location.reload();
+				}
+			});
+		});
 
 		// get Data Ficha_ID form button -> Modal Form_update_ficha
-	    $(".updateDataFicha").click(function () {
+		$(".updateDataFicha").click(function () {
 			var idFicha = $(this).attr('id-ficha');
 
 			// Call AJAX getDataFicha()
-	        $.ajax({
-	            type: 'POST',
-	            url:'?c=Lider&m=getDataFicha',
-				data:{
-					idFicha:idFicha
+			$.ajax({
+				type: 'POST',
+				url: '?c=Lider&m=getDataFicha',
+				data: {
+					idFicha: idFicha
 				},
-	            success(response) {
+				success(response) {
 					var ficha = jQuery.parseJSON(response);
 					$('#txt_num_ficha').val(ficha.num_ficha);
 
@@ -363,7 +365,7 @@
 						allowClear: Boolean($(this).data('allow_clear')),
 						tags: true,
 						dropdownParent: $("#updateDataFicha"),
-					}).val(ficha.id_estado_ficha).trigger('change');
+					});
 
 					// $.ajax({
 					// 	type: 'POST',
@@ -386,7 +388,7 @@
 
 
 
-			        // $('#list-programa').select2({
+					// $('#list-programa').select2({
 					// 	language: "es",
 					// 	theme: "bootstrap",
 					// 	ajax: {
@@ -404,7 +406,7 @@
 					// 		}
 					// 	},
 					// 	tags: true,
-    				// 	dropdownParent: $("#updateDataFicha"),
+					// 	dropdownParent: $("#updateDataFicha"),
 					// });
 
 
@@ -425,54 +427,54 @@
 					// 		});
 					// 	}
 					// }); // End Call AJAX getAllprogramaFormacion()
-	            }
-			});// End Call AJAX getDataFicha()
+				}
+			}); // End Call AJAX getDataFicha()
 
-	    });// End get Data Ficha_ID form button -> Modal Form_update_ficha
+		}); // End get Data Ficha_ID form button -> Modal Form_update_ficha
 
 		// get Data form Button -> Modal Form input
-	    $(".updateDataInstructor").click(function () {
-	        var id_instructor = $(this).attr('id-instructor');
-	        $.ajax({
-	            type: 'POST',
-	            url: '?c=Lider&m=getDataInstructor',
-	            dataType: "json",
-	            data: {
-	                id_instructor: id_instructor
-	            },
-	            success(response) {
-	                var instructor = jQuery.parseJSON(JSON.stringify(response));
-	                $('#text-dni').val(instructor.dni);
-	                $('#text-nombre').val(instructor.nombre);
-	                $('#text-apellido').val(instructor.apellido);
-	                $('#text-email').val(instructor.email);
-	            }
-	        });
-	    });
+		$(".updateDataInstructor").click(function () {
+			var id_instructor = $(this).attr('id-instructor');
+			$.ajax({
+				type: 'POST',
+				url: '?c=Lider&m=getDataInstructor',
+				dataType: "json",
+				data: {
+					id_instructor: id_instructor
+				},
+				success(response) {
+					var instructor = jQuery.parseJSON(JSON.stringify(response));
+					$('#text-dni').val(instructor.dni);
+					$('#text-nombre').val(instructor.nombre);
+					$('#text-apellido').val(instructor.apellido);
+					$('#text-email').val(instructor.email);
+				}
+			});
+		});
 
 		// Agregar nuevo grupo de ficha desde button agregar grupo
-	    $("#btnAgregarGrupo").click(function () {
-	        $.ajax({
-	            type: 'POST',
-	            url: '?c=lider&m=getAllDataFichas',
-	            dataType: "json",
-	            success(response) {
+		$("#btnAgregarGrupo").click(function () {
+			$.ajax({
+				type: 'POST',
+				url: '?c=lider&m=getAllDataFichas',
+				dataType: "json",
+				success(response) {
 					var ficha = jQuery.parseJSON(JSON.stringify(response));
 					console.log(ficha);
-	                //var ficha = removeDuplicates(ficha);
+					//var ficha = removeDuplicates(ficha);
 
-	                var select = $("<select></select>").attr("id", "id_ficha").attr("name", "id_ficha").attr("class", "form-control");
-	                $.each(ficha, function (i, json) {
+					var select = $("<select></select>").attr("id", "id_ficha").attr("name", "id_ficha").attr("class",
+						"form-control");
+					$.each(ficha, function (i, json) {
 						select.append($("<option></option>").attr("value", ficha[i].id_ficha).text(ficha[i].num_ficha));
-	                });
-	                $("#form_select_ficha").html(select);
+					});
+					$("#form_select_ficha").html(select);
 
-	            }
-	        });
+				}
+			});
 		});
 
 	});
-
 </script>
 
 <?php
