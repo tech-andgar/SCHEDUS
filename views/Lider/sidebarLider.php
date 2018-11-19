3f928d32ac6991ec68a6e622803c14b6cd0209c9
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-19 15:59:23 -0500

    Updated Route URL Amigables

--- views/Lider/sidebarLider.php
+++ views/Lider/sidebarLider.php
@@ -3,68 +3,68 @@
         <nav class="sidebar-nav">
             <ul class="nav">
                 <li class="nav-item">
-                    <a class="nav-link active" onclick="LoadUrl('?c=lider')"><i class="nav-icon  icon-speedometer"></i>
+                    <a class="nav-link active" href="index"><i class="nav-icon  icon-speedometer"></i>
                         Escritorio</a>
                 </li>
                 <li class="nav-title">
                     Gestión
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=Horarios')"><i class="nav-icon  fas fa-calendar-alt"></i>
+                    <a class="nav-link" href="Horarios"><i class="nav-icon  fas fa-calendar-alt"></i>
                         Horarios</a>
                 </li>
                 <li class="nav-item nav-dropdown" state-element="close">
                     <a class="nav-link nav-dropdown-toggle"><i class="nav-icon fas fa-code-branch "></i> Ruta Ficha</a>
                     <ul class="nav-dropdown-items">
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Fichas')"><i class="nav-icon  fas fa-graduation-cap"></i>
+                            <a class="nav-link" href="Fichas"><i class="nav-icon  fas fa-graduation-cap"></i>
                                 Fichas</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Grupos')"><i class="nav-icon  fab fa-hubspot"></i>
+                            <a class="nav-link" href="Grupos"><i class="nav-icon  fab fa-hubspot"></i>
                                 Grupos</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Fusionar')"><i class="nav-icon fas fa-project-diagram"></i>
+                            <a class="nav-link" href="Fusionar"><i class="nav-icon fas fa-project-diagram"></i>
                                 Fusionar</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=Instructor')"><i class="nav-icon fas fa-chalkboard-teacher"></i>
+                    <a class="nav-link " href="Instructor"><i class="nav-icon fas fa-chalkboard-teacher"></i>
                         Instructor</a>
                 </li>
                 <li class="nav-item nav-dropdown" state-element="close">
                     <a class="nav-link nav-dropdown-toggle"><i class="nav-icon fas fa-braille"></i> Formacion</a>
                     <ul class="nav-dropdown-items">
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Programas')"><i class="nav-icon  fas fa-book"></i>
+                            <a class="nav-link" href="Programas"><i class="nav-icon  fas fa-book"></i>
                                 Programa</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Niveles')"><i class="nav-icon fas fa-level-up-alt"></i>
+                            <a class="nav-link" href="Niveles"><i class="nav-icon fas fa-level-up-alt"></i>
                                 Niveles</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" onclick="LoadUrl('?c=lider&m=Proyecto')"><i class="nav-icon fas fa-briefcase"></i>
+                            <a class="nav-link" href="Proyecto"><i class="nav-icon fas fa-briefcase"></i>
                                 Proyecto</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=competencias')"><i class="nav-icon fas fa-torah"></i>
+                    <a class="nav-link " href="competencias"><i class="nav-icon fas fa-torah"></i>
                         Competencias</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=TrimestresPrograma')"><i class="nav-icon fas fa-grip-horizontal"></i>
+                    <a class="nav-link " href="TrimestresPrograma"><i class="nav-icon fas fa-grip-horizontal"></i>
                         Trimestre</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=jornadas')"><i class="nav-icon fas fa-cloud-sun"></i>
+                    <a class="nav-link " href="jornadas"><i class="nav-icon fas fa-cloud-sun"></i>
                         Jornada</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link " onclick="LoadUrl('?c=lider&m=ambiente')"><i class="nav-icon fas fa-door-open "></i>
+                    <a class="nav-link " href="ambiente"><i class="nav-icon fas fa-door-open "></i>
                         Ambiente</a>
                 </li>
 
