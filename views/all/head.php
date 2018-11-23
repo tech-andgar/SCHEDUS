a4d4fde468b752af61c5968a1c4c53b3884d5550
Andrés García <afgarcia0479@misena.edu.co>
2018-11-23 15:52:47 -0500

    Fix tempory URL_APP

--- views/all/head.php
+++ views/all/head.php
@@ -178,6 +178,9 @@ if (isset($_SESSION['user'])) {
         function LoadUrl(url) {
             window.location.href = url;
         }
+
+        const APP_URL='<?php echo APP_URL ?>'
+
     </script>
 
 
