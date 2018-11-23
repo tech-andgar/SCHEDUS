7130ef21a1f8f1c2437a55f1df90f0d66f6f0b6a
Andrés García <afgarcia0479@misena.edu.co>
2018-11-23 16:19:05 -0500

    Updated Testing

---
+++ Testing_SCHEDUS/Libs/TempTestCase1543006835079.groovy
@@ -0,0 +1,23 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+import com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\SOPORTE\\AppData\\Local\\Temp\\Katalon\\Test Cases\\CoordinadorLogin\\20181123_160034\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/CoordinadorLogin', new TestCaseBinding('Test Cases/CoordinadorLogin',[:]), FailureHandling.STOP_ON_FAILURE , false)
+    
