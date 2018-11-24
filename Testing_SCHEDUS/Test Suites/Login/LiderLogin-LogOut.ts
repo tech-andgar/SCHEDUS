f9c19ff03d907345323b9b8ef3b415e57c0caf3b
Andrés García <afgarcia0479@misena.edu.co>
2018-11-24 16:58:07 -0500

    Updated Testing Login

---
+++ Testing_SCHEDUS/Test Suites/Login/LiderLogin-LogOut.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>LiderLogin-LogOut</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-11-24T12:18:03</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e77f1941-1767-41ad-a217-801b407c08a9</testSuiteGuid>
+   <testCaseLink>
+      <guid>59420d98-8667-477e-80f8-fb7bf169e43e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login/LiderLogin</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>afd79c7a-ec31-4e80-a36d-8e14f9d0b2f1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/LogOut/LogOutAllUsers</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
