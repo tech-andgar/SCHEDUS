7f30811b2c43b00f81a020c3c65197040acc703b
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-27 12:11:33 -0500

    Views Ambiente
    Views lider Ambiente

--- views/Lider/Sede.php
+++ views/Lider/Sede.php
@@ -4,70 +4,46 @@
     <ol class="breadcrumb">
         <li class="breadcrumb-item">Inicio</li>
         <li class="breadcrumb-item"><a href="#">Lider</a></li>
-        <li class="breadcrumb-item active">Aula de Formacion</li>
+        <li class="breadcrumb-item active">Ambientes de Formacion</li>
     </ol>
     <div class="container-fluid">
         <!-- Ejemplo de tabla Listado -->
         <div class="card">
             <div class="card-header ">
-                <i class="fa fa-align-justify"></i> Aula de Formacion <button type="button" class="btn btn-primary "
+                <i class="fa fa-align-justify"></i> Ambientes de Formacion <button type="button" class="btn btn-primary "
                     data-toggle="modal" data-target="#Agregar">
                     <i class="icon-plus "></i>&nbsp;Nuevo
                 </button>
             </div>
             <div class="card-body">
-                    <table class="table table-responsive-sm table-bordered table-striped table-sm mt-5" id="tableAmbiente">
+                <table class="table table-responsive-sm table-bordered table-striped table-sm mt-5" id="tableAmbiente">
                     <thead>
 					<tr class="success">
-							<th>Ambiente</th>
                             <th>Sede</th>
                             <th>Direccion</th>
-                            <th>Estado</th>
 							<th class="text-center">Actualizar</th>
 						</tr>
 					</thead>
 					<tbody>
-							<?php foreach ($data['ambiente'] as $ambiente) { ?>
-							<tr>
-								<td>
-									<?php echo $ambiente->num_ambiente; ?>
+                    <?php foreach ($data['sede'] as $sede) { ?>
+                        <tr>
+                            <td>
+                                <?php echo $sede->name_sede; ?>
+                            </td>
+                            <td>
+                                <?php echo $sede->direccion; ?>
+                            </td>
+                            <td class="text-center">
+                            <div class="updateDataPrograma" data-toggle="modal" data-target="#Actualizar_Ambiente" id-sede="<?php echo $sede->id_sede; ?>">
+										<i class="far fa-edit fa-lg"></i>
+									</div>
 								</td>
-                                <td>
-									<?php echo $ambiente->name_sede; ?>
-								</td>
-                                <td>
-									<?php echo $ambiente->direccion; ?>
-								</td>
-                                <td>
-                                    <?php
-								    switch ($ambiente->name_estado_ambiente) {
-									case 'Cancelado':
-										echo '<div class="p-2 bg-danger text-white rounded">' . $ambiente->name_estado_ficha . '</div>';
-									break;
-									case 'Completado':
-										echo '<div class="p-2 bg-info text-white rounded">' . $ambiente->name_estado_ficha . '</div>';
-									break;
-									case 'En formación':
-										echo '<div class="p-2 bg-success text-white rounded">' . $ambiente->name_estado_ficha . '</div>';
-									break;
-									case 'Pendiente Asignado':
-										echo '<div class="p-2 bg-warning text-dark rounded">' . $ambiente->name_estado_ficha . '</div>';
-									break;
-									default:
-										echo '<div class="p-2 bg-dark text-white rounded">No pudo obtener estado de formacion ' . $ambiente->name_estado_ficha . '</div>';
-									break;
-								}?>
-								</td>
-								<td class="text-center">
-									<div class="updateDataAmbiente" data-toggle="modal" data-target="#Actualizar_Ambiente" id-ambiente="<?php echo $ambiente->id_ambiente; ?>">
-										<i class="far fa-edit fa-lg"></i></div>
-								</td>
-							</tr>
-							<?php
+                        </tr>
+                        <?php
 						}
 					?>
-						</tbody>
-				</table>
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
@@ -84,25 +60,25 @@
         <div class="modal-content">
             <div class="modal-header">
                 <div class="col-11">
-                    <h3 class=" modal-title text-center">Agregar Nuevo Aula</h3>
+                <h3 class="modal-title text-center">Agregar Nueva Sede</h3>
                 </div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="d-flex justify-content-center ">
+                <div class="d-flex justify-content-center">
                     <form method="post" action="#" class="form-signin form-modal">
                     <div class="container-fluid">
                             <div class="row pt-4">
                                 <div class="col-12">
-                                    <h3> Aula de Formacion</h3>
+                                    <h3> Sede de Formacion</h3>
                                 </div>
                             </div>
                             <div class="row pt-4">
                                 <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Ambiente</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Numero de Ambiente</small>
+                                    <h4>Sede</h4>
+                                    <small id="helpIdNumFicha" class="text-muted">Nombre de la sede</small>
                                 </div>
                                 <div class="col-lg-8 col-12">
                                     <input type="text" name="txt_upd_cod_programa" id="txt_upd_cod_programa" class="form-control">
@@ -110,21 +86,12 @@
                             </div>
                             <div class="row pt-4">
                                 <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Sede</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Nombre de la sede</small>
+                                    <h4 >Dirreccion</h4>
+                                    <small id="helpIdNumFicha" class="text-muted">Dirreccion de sede</small>
                                 </div>
                                 <div class="col-lg-8 col-12">
                                 <select class="form-control" name="" id="" style="width:100%"
 									required></select>
-                                </div>
-                            </div>
-                            <div class="row pt-4">
-                                <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Direccion</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Direccion de la sede</small>
-                                </div>
-                                <div class="col-lg-8 col-12">
-                                    <input type="text" name="txt_upd_cod_programa" id="txt_upd_cod_programa" class="form-control" readonly>
                                 </div>
                             </div>
                         </div>
@@ -143,24 +110,26 @@
     <div class="modal-dialog modal-dialog-centered modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h3 class="col-11 modal-title text-center">Actualizar Datos</h3>
+                <div class="col-11">
+                    <h3 class=" modal-title text-center">Actualizar Datos</h3>
+                </div>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
-                <div class="d-flex justify-content-center text-center">
+                <div class="d-flex justify-content-center ">
                     <form method="post" action="#" class="form-signin form-modal">
                     <div class="container-fluid">
                             <div class="row pt-4">
                                 <div class="col-12">
-                                    <h3> Aula de Formacion</h3>
+                                    <h3> Sede de Formacion</h3>
                                 </div>
                             </div>
                             <div class="row pt-4">
                                 <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Ambiente</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Numero de Ambiente</small>
+                                    <h4>Sede</h4>
+                                    <small id="helpIdNumFicha" class="text-muted">Nombre de la sede</small>
                                 </div>
                                 <div class="col-lg-8 col-12">
                                     <input type="text" name="txt_upd_cod_programa" id="txt_upd_cod_programa" class="form-control">
@@ -168,21 +137,12 @@
                             </div>
                             <div class="row pt-4">
                                 <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Sede</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Nombre de la sede</small>
+                                    <h4 >Dirreccion</h4>
+                                    <small id="helpIdNumFicha" class="text-muted">Dirreccion de sede</small>
                                 </div>
                                 <div class="col-lg-8 col-12">
                                 <select class="form-control" name="" id="" style="width:100%"
 									required></select>
-                                </div>
-                            </div>
-                            <div class="row pt-4">
-                                <div class="col-lg-4 col-12">
-                                    <h4 for="codig">Direccion</h4>
-                                    <small id="helpIdNumFicha" class="text-muted">Direccion de la sede</small>
-                                </div>
-                                <div class="col-lg-8 col-12">
-                                    <input type="text" name="txt_upd_cod_programa" id="txt_upd_cod_programa" class="form-control" readonly>
                                 </div>
                             </div>
                         </div>
