4fe636c752311dafe03ab8d92c5194860818ced2
Andrés García <afgarcia0479@misena.edu.co>
2018-11-27 18:02:54 -0500

    Fix method UpdateDataProgramaFormacion and changeStatus

--- controllers/LiderController.php
+++ controllers/LiderController.php
@@ -395,30 +395,34 @@ class LiderController extends Path
 
     public function updateDataProgramaFormacion()
     {
-        $data = array(
-            "txto_cod_programa" => $_POST['txto_cod_programa'],
-            "txto_short_name_programa" => $_POST['txto_short_name_programa'],
-            "txto_name_programa_formacion" => $_POST['txt_name_programa_formacion'],
-            "txto_version_programa" => $_POST['txt_version_programa'],
-            "txto_id_nivel_programa_formacion" => $_POST['txt_id_nivel_programa_formacion'],
-            "txto_cod_proyecto" => $_POST['txt_cod_proyecto'],
-        );
-        $result = $this->modelProgramaFormacion->updateDataProgramaFormacion($data);
-        if ($result) {
-            $msgType = array(
-                'type' => 'success',
-                'title' => 'AVISO',
-                'msg' => 'Exito actualizado datos de Programa de Formacion',
+
+        if (isset($_POST)) {
+            $data = array(
+                "txtUpdIdPrograma" => $_POST['txt_upd_id_programa'],
+                "txtUpdCodPrograma" => $_POST['txt_upd_cod_programa'],
+                "txtUpdShortNamePrograma" => $_POST['txt_upd_short_name_programa'],
+                "txtUpdNameProgramaFormacion" => $_POST['txt_upd_name_programa_formacion'],
+                "txtUpdVersionPrograma" => $_POST['txt_upd_version_programa'],
+                "txtUpdIdNivelProgramaFormacion" => $_POST['txt_upd_id_nivel_programa_formacion'],
+                "txtUpdCodProyecto" => $_POST['txt_upd_cod_proyecto'],
             );
-        } else {
-            $msgType = array(
-                'type' => 'error',
-                'title' => 'AVISO',
-                'msg' => 'No se pudo actualizar Programa de Formacion',
-            );
+            $result = $this->modelProgramaFormacion->updateDataProgramaFormacion($data);
+            if ($result) {
+                $msgType = array(
+                    'type' => 'success',
+                    'title' => 'AVISO',
+                    'msg' => 'Exito actualizado datos de Programa de Formacion',
+                );
+            } else {
+                $msgType = array(
+                    'type' => 'error',
+                    'title' => 'AVISO',
+                    'msg' => 'No se pudo actualizar Programa de Formacion',
+                );
+            }
+            $this->Programas($msgType);
         }
 
-        $this->Programas($msgType);
     }
 
     public function changeStatusProgaramaFormaciom()
@@ -435,22 +439,7 @@ class LiderController extends Path
             "cod_estado_programa_formacion" => $status,
         );
 
-        $result = $this->modelProgramaFormacion->updateStatusProgramaFormacion($data);
-        // if ($result) {
-        //     $msgType = array(
-        //         'type' => 'success',
-        //         'title' => 'AVISO',
-        //         'msg' => 'Exito actualizado estado de programa de formación',
-        //     );
-        // } else {
-        //     $msgType = array(
-        //         'type' => 'error',
-        //         'title' => 'AVISO',
-        //         'msg' => 'No se pudo actualizar estado de programa de formación',
-        //     );
-        // }
-
-        // $this->Programas($msgType);
+        $this->modelProgramaFormacion->updateStatusProgramaFormacion($data);
     }
 
     //
