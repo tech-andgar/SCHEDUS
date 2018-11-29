a874df84d8818b1beaba27b5bd0d0cd57f41808e
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-29 17:31:35 -0500

    Models
    Configuracion 
    Competecias
    Jornada
    NivelProgramaFormacion
    preparedSQL
    Proyecto

--- models/JornadaModel.php
+++ models/JornadaModel.php
@@ -85,11 +85,11 @@ class JornadaModel extends DB {
         }
 	}
 	
-	public function getJornadaId($idNivelFormacion)
+	public function getJornadaId($idJornada)
     {
         try {
             $stm = parent::conectar()->prepare(preparedSQL::GET_JORNADA_ID);
-            $stm->bindParam(1, $idNivelFormacion, PDO::PARAM_STR);
+            $stm->bindParam(1, $idJornada, PDO::PARAM_STR);
             $stm->execute();
             return $stm->fetch(PDO::FETCH_OBJ); // Retorno data Jornadas
         } catch (Exception $e) {
@@ -97,15 +97,56 @@ class JornadaModel extends DB {
         }
     }
 
-	public function getJornadaName($nameNivelFormacion)
+	public function getJornadaName($nameJornada)
     {
         try {
 			$stm = parent::conectar()->prepare(preparedSQL::GET_JORNADA_NAME);
-            $nameNivelFormacion = '%'.$nameNivelFormacion.'%';
-            $stm->bindParam(1, $nameNivelFormacion, PDO::PARAM_STR);
+            $nameJornada = '%'.$nameJornada.'%';
+            $stm->bindParam(1, $nameJornada, PDO::PARAM_STR);
             $stm->execute();
             return $stm->fetchAll(PDO::FETCH_OBJ); // Retorno data de seleccionadas lista de Jornadas
         } catch (Exception $e) {
+            die($e->getMessage());
+        }
+	}
+	
+	public function insertarJornada(array $data)
+    {
+		// var_dump($_POST);
+        try {
+			// Verificar hay datos en $data
+            if (
+                isset($data['name_jornada'])) {
+                $name_jornada = $data['name_jornada'];
+                $stm = parent::conectar()->prepare(preparedSQL::INSERT_NEW_JORNADA);
+                $stm->bindParam(1, $name_jornada, PDO::PARAM_STR);
+                $stm->execute();
+                return true;
+            }
+        } catch (Exception $e) {
+            return false;
+            die($e->getMessage());
+        }
+	}
+	
+
+	public function updateDataJornada($data)
+    {
+        try {
+			/* var_dump($data); */
+            // Verificar hay datos en $data
+			if (isset($data['id_jornada']) &&
+				isset($data['name_jornada'])) {
+                $name_jornada = $data['name_jornada'];
+                $id_jornada = $data['id_jornada'];
+                $stm = parent::conectar()->prepare(preparedSQL::UPDATE_DATA_JORNADA_ID);
+                $stm->bindParam(1, $name_jornada, PDO::PARAM_STR);
+                $stm->bindParam(2, $id_jornada, PDO::PARAM_STR);
+                $stm->execute();
+                return true;
+            }
+        } catch (Exception $e) {
+            return false;
             die($e->getMessage());
         }
     }
