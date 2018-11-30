8559db1ba7370dc627b359b01294ffd8531a08e3
JOHANN AMAYA LOPEZ <jaamaya643@misena.edu.co>
2018-11-30 08:42:02 -0500

    Views lider Niveles
    Views Niveles

--- views/Lider/Niveles.php
+++ views/Lider/Niveles.php
@@ -208,10 +208,8 @@
 			//	'pdfHtml5'
 			//]
 		});
-
-
-
 	});
+
 		$(".updateDataNivelFormacion").click(function () {
 			var idNivelFormacion = $(this).attr('id-NivelFormacion');
 			console.log(idNivelFormacion);
