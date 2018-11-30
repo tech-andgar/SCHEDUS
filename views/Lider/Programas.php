124c98ab13866c9768d69856b88f8ecb8de61025
Andrés García <afgarcia0479@misena.edu.co>
2018-11-30 16:15:35 -0500

    Delete lines unneed script on Programa

--- views/Lider/Programas.php
+++ views/Lider/Programas.php
@@ -122,7 +122,7 @@
 									<h4 for="txt_name_programa_formacion">Nombre del programa</h4>
 								</div>
 								<div class="col-lg-6 col-12">
-									<input type="text" name="txt_name_programa_formacion" id="txt_name_programa_formacion"class="form-control" aria-describedby="helpNameProgramaFormacion"  pattern="[a-zA-Z]{1,191}" title="Unicamente letras" required>
+									<input type="text" name="txt_name_programa_formacion" id="txt_name_programa_formacion"class="form-control" aria-describedby="helpNameProgramaFormacion" pattern="([A-z0-9À-ž\s]){2,191}" title="Unicamente letras" required>
 									<small id="helpNameProgramaFormacion" class="form-text text-muted">Escriba programa de formación(Unicamente letras)</small>
 								</div>
 							</div>
@@ -224,7 +224,7 @@
 									<h4 for="txt_upd_name_programa_formacion">Nombre del Programa</h4>
 									</div>
 								<div class="col-lg-6 col-12">
-									<input type="text" id="txt_upd_name_programa_formacion" class="form-control" name="txt_upd_name_programa_formacion" aria-describedby="helpUpdNameProgramaFormacion" pattern="[a-zA-Z]{1,191}" title="Unicamente letras" required>
+									<input type="text" id="txt_upd_name_programa_formacion" class="form-control" name="txt_upd_name_programa_formacion" aria-describedby="helpUpdNameProgramaFormacion" pattern="([A-z0-9À-ž\s]){2,191}" title="Unicamente letras" required>
 									<small id="helpUpdNameProgramaFormacion" class="form-text text-muted">Escriba programa de formación(Unicamente letras)</small>
 								</div>
 							</div>
@@ -284,59 +284,6 @@
 
 <script>
 	$(document).ready(function () {
-
-	//
-	// ─── TABLE LOAD ─────────────────────────────────────────────────────────────────
-	//
-
-		$("#tableProgramas").DataTable({
-			"language": {
-				"sProcessing": "Procesando...",
-				"sLengthMenu": "Mostrar _MENU_ registros",
-				"sZeroRecords": "No se encontraron resultados",
-				"sEmptyTable": "Ningún dato disponible en esta tabla",
-				"sInfo": "Registros del _START_ al _END_ de un total de _TOTAL_ registros",
-				"sInfoEmpty": "Registros del 0 al 0 de un total de 0 registros",
-				"sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
-				"sInfoPostFix": "",
-				"sSearch": "Buscar:",
-				"sUrl": "",
-				"sInfoThousands": ",",
-				"sLoadingRecords": "Cargando...",
-				"oPaginate": {
-					"sFirst": "Primero",
-					"sLast": "Último",
-					"sNext": "Siguiente",
-					"sPrevious": "Anterior"
-				},
-				"oAria": {
-					"sSortAscending": ": Activar para ordenar la columna de manera ascendente",
-					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
-				}
-			},
-			"dom": // Insertar objeto tabla por formato:
-				// Encabezado de la tabla -- l->Num registros por pagina, f-> barra de filtro
-				"<'row'<'col-sm-6'f><'col-sm-6'l>>" +
-				// Cuerpo de la tabla -- t-> tabla, r (no aun entiendo)
-				"<'row'<'col-sm-12 table-responsive d-flex justify-content-center'tr>>" +
-				// Seccion estado de la tabla -- i-> info de tabla, p-> num Paginas por dividir registros
-				"<'row'<'col-sm-4'><'col-sm-8'i><'col-sm-4'><'col-sm-6'p>>" +
-				// Pie de la tabla -- B-> Botones de exportar
-				"<'row'<'col-sm-12'B>>",
-			
-			//buttons: [
-			//	'copyHtml5',
-			//	'excelHtml5',
-			//	'csvHtml5',
-			//	'pdfHtml5'
-			//]
-		});
-	//
-	// ─── END TABLE LOAD ─────────────────────────────────────────────────────────────────
-	//
-
-
-
 	//
 	// ─── CALL AJAX GETDATANIVELPROGRAMAFORMACION TO SELECT ───────────
 	//
@@ -551,7 +498,7 @@
 
 	});
 
-	
+
 	//
 	// ─── CLICK BTN CHANGESTATUSPROGARAMAFORMACIOM ───────────────────────────────────
 	//
@@ -585,6 +532,6 @@
 	// 	event.target.setCustomValidity('Maximo de digitos 10');
 	// }
 
-	
+
 
 </script>
