fa26e5c962ee296a37ae4fe9c42ec396be18f50a
Andrés García <afgarcia0479@misena.edu.co>
2018-12-03 17:49:13 -0500

    [WIP] mailSend of recovery Password

--- controllers/MailController.php
+++ controllers/MailController.php
@@ -2,71 +2,84 @@
 
 use PHPMailer\PHPMailer\PHPMailer;
 
-function sendemail($mail_addAddress, $email_subject)
+function sendEmail($data)
 {
-    $mail = new PHPMailer();
 
-    // Establecer el correo electronico para utilizar SMTP
-    $mail->isSMTP();
+    try {
 
-   // Especificar el servidor de correo a utilizar
-    $mail->Host = 'smtp.gmail.com';
+        $mail = new PHPMailer();
 
-    // Habilitar la autenticacion con SMTP
-    $mail->SMTPAuth = true;
+        // Establecer el correo electronico para utilizar SMTP
+        $mail->isSMTP();
 
-    // Habilitar encriptación, 'ssl' es aceptada
-    $mail->SMTPSecure = 'tls';
+        // Especificar el servidor de correo a utilizar
+        $mail->Host = 'smtp.gmail.com';
 
-    // Puerto TCP para conectarse
-    $mail->Port = 587;
+        // Habilitar la autenticacion con SMTP
+        $mail->SMTPAuth = true;
 
-    /* Configuracion de variables para enviar el correo */
-    $mail_setFromName = 'Soporte SCHEDUS';
-    // $mail_setFromEmail = '8807c37c627601';
-    // $mail_setFromPassword = 'd5671c9d3ee424';
-    $mail_setFromEmail = 'soportenewreset@gmail.com';
-    $mail_setFromPassword = 'AG40DA48CT41';
+        // Habilitar encriptación, 'ssl' es aceptada
+        $mail->SMTPSecure = 'tls';
+
+        // Puerto TCP para conectarse
+        $mail->Port = 587;
+
+        /* Configuracion de variables para enviar el correo */
+        $mail_setFromName = 'Soporte SCHEDUS';
+        // $mail_setFromEmail = '8807c37c627601';
+        // $mail_setFromPassword = 'd5671c9d3ee424';
+        $mail_setFromEmail = 'soportenewreset@gmail.com';
+        $mail_setFromPassword = 'AG40DA48CT41';
 
 
-    // Correo electronico saliente ejemplo: tucorreo@email.com
-    $mail->Username = $mail_setFromEmail;
+        // Correo electronico saliente ejemplo: tucorreo@email.com
+        $mail->Username = $mail_setFromEmail;
 
-    // Contraseña de correo saliente
-    $mail->Password = $mail_setFromPassword;
+        // Contraseña de correo saliente
+        $mail->Password = $mail_setFromPassword;
 
-    //Introduzca la direccion de la que debe aparecer el correo electronico. Puede utilizar cualquier direccion que el servidor SMTP acepte como valida. El segundo parametro opcional para esta funcion es el nombre que se mostrara com el remitente en lugar de la direccion de correo electronico en si.
-    $mail->setFrom($mail_setFromEmail, $mail_setFromName);
+        //Introduzca la direccion de la que debe aparecer el correo electronico. Puede utilizar cualquier direccion que el servidor SMTP acepte como valida. El segundo parametro opcional para esta funcion es el nombre que se mostrara com el remitente en lugar de la direccion de correo electronico en si.
+        $mail->setFrom($mail_setFromEmail, $mail_setFromName);
 
-    // Agregar quien recibe el email enviado
-    $mail->addAddress($mail_addAddress);
+        // Agregar quien recibe el email enviado
+        $mail->addAddress($data['user']->email);
 
-    // Activo condificacción utf-8
-    $mail->CharSet = 'UTF-8';
+        // Activo condificacción utf-8
+        $mail->CharSet = 'UTF-8';
 
-    //$message = $txt_message;
-    $mail->Subject = 'Restablecer de contraseña SCHEDUS';
-    $message = 'Puede ingresar al aplicativo en el siguiente enlace solo en la sede Barrio Colombia: http://localhost/schedus?c=inicio&m=fromEmail <br> Su Contraseña Es: 8XRXYVH3';
-    //$message+= file_get_contents($template);
-    //$message = str_replace('{{first_name}}', $mail_setFromName, $message);
-    //$message = str_replace('{{customer_email}}', $mail_setFromEmail, $message);
+        //$message = $txt_message;
+        $mail->Subject = $data['mail_subject'];
+        // $message = 'Puede ingresar al aplicativo en el siguiente enlace solo en la sede Barrio Colombia: http://localhost/schedus?c=inicio&m=fromEmail <br> Su Contraseña Es: 8XRXYVH3';
+        $message = file_get_contents($data['path_template']);
 
-    //Establecer el formato de correo electronico en HTML
-    $mail->isHTML(true);
+        $url_path = APP_URL.'resetPassword/'. $data['user']->email.'\;'.$data['user']->token;
+        $message = str_replace('{{nombre}}', $data['user']->nombre, $message);
+        $message = str_replace('{{apellido}}', $data['user']->apellido, $message);
+        $message = str_replace('{{action_url}}', $$url_path, $message);
 
-    //$mail->Subject = $email_subject;
-    $mail->msgHTML($message);
-    //$mail->Body    = 'This is the HTML message body <b>in bold!</b><br>';
-    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        //Establecer el formato de correo electronico en HTML
+        $mail->isHTML(true);
 
-    // To load the Spanish version
-    $mail->setLanguage('es', '../../assets/Framework/PHPMailer/');
+        //$mail->Subject = $email_subject;
+        $mail->msgHTML($message);
+        //$mail->Body    = 'This is the HTML message body <b>in bold!</b><br>';
+        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-    if (!$mail->send()) {
-        echo '<p style="color:red"> No se pudo enviar el mensaje..';
-        echo '<br>Error de correo:' . $mail->ErrorInfo;
-        echo '</p>';
-    } else {
-        echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
+        // To load the Spanish version
+        $mail->setLanguage('es', '../../assets/Framework/PHPMailer/');
+
+        if (!$mail->send()) {
+            throw new Exception("<p style='color:red'> No se pudo enviar el mensaje <br> Error de correo: $mail->ErrorInfo</p>");
+            return false;
+        } else {
+            return true;
+            // echo '<p style="color:green">Tu mensaje ha sido enviado!</p>';
+        }
+
+    } catch (Exception $e) {
+        echo "Message : " . $e->getMessage();
+        echo "<br>Code : " . $e->getCode();
     }
+
+
 }
--- views/Inicio/header.php
+++ views/Inicio/header.php
@@ -24,7 +24,11 @@
                             </div>
                         </form>
                     </div>
-                    <a href="#" data-toggle="modal" data-target="#modalRecuperarContrasena">¿Olvidó la contraseña?</a>
+                    <hr>
+                    <div class="text-center pt-2">
+                    <a href="#" data-toggle="modal" data-target="#modalRecuperarContrasena" class="forgot-password">
+                        ¿Olvidó la contraseña?
+                    </a></div>
                 </li>
             </ul>
         </li>
@@ -33,7 +37,7 @@
 
 <!-- Modal -->
 <div class="modal fade" id="modalRecuperarContrasena" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-dialog modal-dialog-centered " role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h3 class="col-11 modal-title text-center">Recuperar contraseña</h3>
@@ -43,10 +47,18 @@
             </div>
             <div class="modal-body">
                 <div class="d-flex justify-content-center text-center">
-                    <form method="POST" action="?c=inicio&m=sendMailRecoveryPassword" class="form-signin">
-                        <div class="form-group">
-                            <h5>Correo electronico</h5>
-                            <input type="email" class="form-control adsi-css" name="emailRecovery" placeholder="ejemplo@e-mail.com" required/>
+                    <form method="POST" action="<?php echo APP_URL ?>Inicio/sendMailRecoveryPassword"class="form-signin form-modal">
+                        <div class="container-fluid">
+                            <div class="row pt-4">
+                                <div class="col-lg-4 col-12 ">
+                                    <h4>Correo electronico</h4>
+                                    <small id="" class="text-muted">Escriba Correo Electronico
+                                    </small>
+                                </div>
+                                <div class="col-lg-8 col-12">
+                                    <input type="email" class="form-control" name="emailRecovery" placeholder="ejemplo@e-mail.com" required />
+                                </div>
+                            </div>
                         </div>
                         <hr>
                         <div class="text-center pt-2">
